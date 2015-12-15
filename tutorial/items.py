84ebcf8f7eb0fc523fd67faca2e8923327f44117
ball <ms0407954@gmail.com>
2015-12-15 21:11:07 +0800

    test1

---
+++ tutorial/items.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TutorialItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class DmozItem(scrapy.Item):
+    title = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.Field()
+
+class TorrentItem(scrapy.Item):
+    url = scrapy.Field()
+    name = scrapy.Field()
+    description = scrapy.Field()
+    size = scrapy.Field()
+
+class BaItem(scrapy.Item):
+    name = scrapy.Field()
+
+
+
